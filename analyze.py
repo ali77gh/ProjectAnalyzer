a8c191d89191c5d4d4c048e8c7dc577c27255c3a
ali <alighahremani1377@gmail.com>
2018-06-30 21:22:26 +0430

    FileType object done (not tested)

--- analyze.py
+++ analyze.py
@@ -1,18 +1,55 @@
-import os;
+# i write all codes on 1 file becuse its easy to move for users ;)
+import os
 
-formatList = []
+allFiles = []
+fileTypes = []
+debug = 1  # 1: enable logs
 
-class Data :
-    def __init__(self , formats):
-        self.keys = format
-        self.values = []
-        print("data object created on RAM")
+
+class FileType:
+    # dotSth is string like -> "js" , "py" ,...
+    def __init__(self, dotSth):
+        self.key = dotSth
+        self.filesNum = 0  # how match files
+        self.linesNum = 0  # how match lines
+        self.linesAvg = 0  # TODO average lines of code in one file
+        LogIt("FileType " + dotSth + "object created")
+
+    # returns bool
+    def isKeyMatch(self, nkey):
+        return self.key == nkey
+
+    def getFilePathDotSth(self, filePath):
+        nothing, dotSth = filePath.split(".")
+        return dotSth
+
+    def safeAddFile(self, filePath):
+        # check dot sth
+        dotSth = self.getFilePathDotSth(filePath)
+        if not self.isKeyMatch(dotSth):
+            return
+        else:
+            # is match
+            self.filesNum += 1
+            self.linesNum = getLineNumber(filePath)
+            LogIt(self.key + " files num:" + self.filesNum + " lines: " + self.linesNum)
+
+    def safeAddFiles(self, filePathsList):
+        for i in filePathsList:
+            self.safeAddFile(i)
+
 
 def getDirContent():
     return os.listdir(os.getcwd())
 
+
+def LogIt(message):
+    if debug:
+        print("Log : -> ", message)
+
+
 def getLineNumber(fileName):
-    fp = open(fileName, 'r')  
+    fp = open(fileName, 'r')
     line = fp.readline()
     cnt = 0
     while line:
@@ -20,29 +57,29 @@ def getLineNumber(fileName):
         cnt += 1
     return cnt
 
+
 print(30*"-")
-print("enter postfix of file  ")
-print("example -> py")
+print("postfix like -> py , js , java , cpp ...")
 print("enter con to continue")
 print(30*"-")
 
+#TODO : get dot sths as argmants if user do not enter run blow code
+#read dot sth's
 exitLoop = False
 while True:
     newformat = input("enter postfix : ")
-    if newformat=="con":
+    if newformat == "con":
         break
-    formatList.append(newformat)
-
-print(formatList)
+    fileTypes.append(FileType(newformat))
 
 rootContext = getDirContent()
 print(rootContext)
 
 for i in rootContext:
     try:
-        cnt= getLineNumber(i)
+        cnt = getLineNumber(i)
         print(cnt)
     except:
         pass
 
-data =Data()
+d = FileType("js")
