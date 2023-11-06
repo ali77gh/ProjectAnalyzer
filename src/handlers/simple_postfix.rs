cfbf5e2053da7cf6f64a5783b64cd8c0ca0f2064
Ali ghahremani <alighahremani1377@gmail.com>
2023-11-06 14:45:01 +0330

    improve: 2 times faster scan by using threads

--- src/handlers/simple_postfix.rs
+++ src/handlers/simple_postfix.rs
@@ -1,24 +1,47 @@
-use std::{path::Path, fs::DirEntry};
+use std::{path::Path, fs::DirEntry, sync::mpsc, thread};
 use crate::utils::walker::walk;
 use std::fs;
+use std::path::PathBuf;
 
-pub fn simple_postfix_handler(postfix: &String){
+pub fn simple_postfix_handler(postfix: &str){
     let mut file_counter: usize = 0;
     let mut line_counter: usize = 0;
 
-    let filter_by = format!(".{}",postfix);
+    let thread_postfix = postfix.to_owned();
+    let banner_postfix = postfix.to_owned();
 
-    let mut cb = |x:&DirEntry|{
-        if x.file_name().to_str().unwrap().ends_with(&filter_by){
-            file_counter+=1;
-            let content = fs::read_to_string(x.path())
-                .unwrap_or_else(|_|{println!("can't read file:{}",x.path().to_str().unwrap()); "".to_string()});
+    let (tx12, rx12) = mpsc::channel::<Option<PathBuf>>(); // channel thread 1,2
+    thread::spawn(move ||{
+        let filter_by = format!(".{}", thread_postfix);
+        let mut cb = |x:&DirEntry|{
+            if x.file_name().to_str().unwrap().ends_with(&filter_by){
+                tx12.send(Some(x.path())).unwrap();
+            }
+        };
+        let _ = walk(Path::new("./"), &mut cb);
+        tx12.send(None).unwrap();
+    });
 
-            line_counter += content.split("\n").count()-1;
+    let (tx23, rx23) = mpsc::channel::<Option<Vec<u8>>>();  // channel thread 2,3
+    // file provider thread
+    thread::spawn(move || {
+        while let Some(x) = rx12.recv().unwrap(){
+            let content = fs::read(x.clone())
+                .unwrap_or_else(|_|{ 
+                    println!("can't read file:{}", x.to_str().unwrap()); vec![]
+            });
+            tx23.send(Some(content)).unwrap();
         }
-    };
+        tx23.send(None).unwrap();
+    });
 
-    let _ = walk(Path::new("./"), &mut cb);
+    // counter thread
+    let nl = b'\n';
+    let nl = &&nl; // pre process dereference
+    while let Some(x) = rx23.recv().unwrap(){
+        file_counter += 1;
+        line_counter += x.iter().filter(|byte|byte==nl).count();
+    }
 
     let division = line_counter/file_counter;
 
@@ -28,9 +51,9 @@ pub fn simple_postfix_handler(postfix: &String){
     println!("│ https://github.com/ali77gh/ProjectAnalyzer    │");
     println!("│                                               │");
     println!("│ searching...                                  │");
-    println!("│ you have {} {} files           \t\t│",file_counter,postfix);
-    println!("│ you have {} lines of {}      \t\t│",line_counter,postfix);
-    println!("│ lines per file average: {}       \t\t│",division);
+    println!("│ you have {} {} files           \t\t│", file_counter,banner_postfix);
+    println!("│ you have {} lines of {}      \t\t│", line_counter,banner_postfix);
+    println!("│ average lines per file: {}       \t\t│", division);
     println!("│                                               │");
     println!("└───────────────────────────────────────────────┘");
 
--- src/input_parser.rs
+++ src/input_parser.rs
@@ -12,10 +12,9 @@ pub enum RequestType{
 pub fn parse_input() -> RequestType {
     let args: Vec<String> = env::args().collect();
     
-
     if let Some(arg1) = args.get(1){
         if arg1 == "--help" || arg1=="-h"{
-            return RequestType::Help;
+            RequestType::Help
         } else if arg1 == "--version" || arg1=="-v" {
             return RequestType::Version;
         } else if arg1 == "--update" || arg1 == "-u"{
@@ -26,6 +25,6 @@ pub fn parse_input() -> RequestType {
             return RequestType::SimplePostfix(arg1.clone());
         }
     }else{
-        return RequestType::Help;
+        RequestType::Help
     }
 }
