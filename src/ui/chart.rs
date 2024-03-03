b4a765cfcb72ba2103967ff4bcca9c7718a6cdb2
Ali ghahremani <alighahremani1377@gmail.com>
2024-03-03 11:12:12 +0330

    fix: chart panic

--- src/ui/chart.rs
+++ src/ui/chart.rs
@@ -15,14 +15,17 @@ impl DrawableChart for AnalyzeResult {
         let mut dataset = vec![];
         let mut counter = 0;
         for item in self.iter() {
-            let style: Style = (*COLORS.get(counter).unwrap()).into();
+            let style: Style = (get_rounded(&COLORS, counter)).into();
             dataset.push(Data {
                 label: item.name().to_string(),
                 value: item.lines() as f32,
                 color: Some(style),
-                fill: *CHARS.get(counter).unwrap(),
+                fill: get_rounded(&CHARS, counter),
             });
             counter += 1;
+        }
+        if counter > 10 {
+            counter = 10;
         }
         // draw chart
         Chart::new()
@@ -33,12 +36,16 @@ impl DrawableChart for AnalyzeResult {
     }
 }
 
-const CHARS: [char; 28] = [
-    '▰', '▼', '◆', '■', '•', '▪', '▴', '▰', '▼', '◆', '■', '•', '▪', '▴', '▰', '▼', '◆', '■', '•',
-    '▪', '▴', '▰', '▼', '◆', '■', '•', '▪', '▴',
-];
+fn get_rounded<T: Clone>(array: &[T], index: usize) -> T {
+    let mut index = index;
+    while index >= array.len() {
+        index -= array.len();
+    }
+    array[index].clone()
+}
+const CHARS: [char; 7] = ['▰', '▼', '◆', '■', '•', '▪', '▴'];
 
-const COLORS: [Color; 18] = [
+const COLORS: [Color; 7] = [
     Color::Green,
     Color::Yellow,
     Color::Blue,
@@ -46,15 +53,4 @@ const COLORS: [Color; 18] = [
     Color::Cyan,
     Color::White,
     Color::Red,
-    Color::Green,
-    Color::Yellow,
-    Color::Blue,
-    Color::Purple,
-    Color::Cyan,
-    Color::White,
-    Color::Red,
-    Color::Green,
-    Color::Yellow,
-    Color::Blue,
-    Color::Purple,
 ];
